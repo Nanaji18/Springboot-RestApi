1de546ec0487a93f6f5ddd6b4ac2236620296582
Nanaji18 <appalarajumorukurthi@gmail.com>
2025-04-19 10:05:17 +0530

    Initial commit

---
+++ src/main/java/com/example/restapi/Springboor/Rest/API/SpringboorRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapi.Springboor.Rest.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboorRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboorRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapi/Springboor/Rest/API/SpringboorRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapi.Springboor.Rest.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboorRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
